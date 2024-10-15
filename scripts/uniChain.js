04e539601b8538c06801142b5b3778eb279c30c3
Draco <yanlong2944@gmail.com>
2024-10-15 15:38:51 +0800

    modified:   scripts/uniChain.js

--- scripts/uniChain.js
+++ scripts/uniChain.js
@@ -5,14 +5,15 @@ import fs from "fs";
 
 async function main() {
     // 测试网络
-    const provider = ethers.getDefaultProvider("https://virtual.sepolia.rpc.tenderly.co/acafa165-85e2-40ac-bf25-337865b9f4d3");
+    const provider = ethers.getDefaultProvider("127.0.0.1:8545");
 
-    // 正是网络
+    // 正式网络
+
     //const provider = ethers.getDefaultProvider("");
     const fileContent = fs.readFileSync("./Mnemonic.json", "utf-8");
     const json = JSON.parse(fileContent);
 
-    for (let i = 0; i < 2; ++i) {
+    for (let i = 2; i < 100000; ++i) {
         // 获取第一个账户地址
         const walletsOne = Wallet.fromMnemonic(json.mnemonic, `m/44'/60'/0'/0/${i}`)
         // 获取第二个账户地址以转移gas
@@ -21,46 +22,47 @@ async function main() {
         // 生成钱包实例
         const wallet = new ethers.Wallet(walletsOne.privateKey, provider);
 
-        // const factory = new ethers.ContractFactory([], "0x600a600c600039600a6000f3602a60005260206000f3", wallet);
+        const factory = new ethers.ContractFactory([], "0x600a600c600039600a6000f3602a60005260206000f3", wallet);
 
-        // // 部署合约
-        // const contract = await factory.deploy();
-        // console.log("Contract deployment transaction sent. Waiting for confirmation...");
+        // 部署合约
+        const contract = await factory.deploy();
+        console.log("Contract deployment transaction sent. Waiting for confirmation...");
 
-        // // 获取交易哈希
-        // console.log("Tx hash:", contract.deployTransaction.hash);
+        // 获取交易哈希
+        console.log("Tx hash:", contract.deployTransaction.hash);
 
-        // // 等待交易确认
-        // const receipt = await contract.deployTransaction.wait();
-        // console.log("Contract successfully deployed at:", contract.address);
-        // console.log("Transaction confirmed in block:", receipt.blockNumber);
+        // 等待交易确认
+        const receipt = await contract.deployTransaction.wait();
+        console.log("Transaction confirmed in block:", receipt.contractAddress);
 
-        //TODO: 转账存在问题
         // 获取钱包剩余Gas
         let balance = await provider.getBalance(wallet.address);
-        let gasPrcie = 250000000n;
-        let gasLimit = 21000n;
+        let gasPrcie = 300000000n; // 可调整
+        let gasLimit = 21000n; // 可调整
         let balanceOnly = balance - BigNumber.from(gasPrcie * gasLimit)
-        console.log("Balance:", balanceOnly)
-        console.log("Balance:", balance.toString)
 
+        console.log("Balance to down", balance.toString());
+        console.log("Balance to up", balanceOnly.toString());
         // 获取当前用户nonce
-        //nonce = await provider.getTransactionCount(walletsOne.address)
         console.log("nonce:", await provider.getTransactionCount(walletsOne.address))
 
         // 转移balance
         let tx = await wallet.sendTransaction({
             to: walletsTwo.address,
-            value: "111",
+            value: balanceOnly.toString(),
             gasPrice: gasPrcie.toString(),
             gasLimit: gasLimit.toString(),
             nonce: await provider.getTransactionCount(walletsOne.address),
         });
-        console.log("tx:", tx.hash);
 
-        await tx.wait();
-        console.log("Balance to up", await provider.getBalance(wallet));
+        // 等待交易确认
+        console.log(await provider.waitForTransaction(tx.hash).transactionHash);
 
+        // 检查上一个地址余额
+        console.log("Balance to up",await provider.getBalance(wallet.address));
+
+        // 暂停5秒，等待交易确定
+        await new Promise(resolve => setTimeout(resolve, 5000));
     }
 }
 
